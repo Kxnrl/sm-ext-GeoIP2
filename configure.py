a56fe62bc13e5daf5a358d862fb66a6115adf3a8
Kyle <kyle@kxnrl.com>
2022-01-12 05:09:53 +0800

    update configure py

--- configure.py
+++ configure.py
@@ -4,27 +4,20 @@ from ambuild2 import run
 
 # Simple extensions do not need to modify this file.
 
-ambuild_version = getattr(run, 'CURRENT_API', '2.1')
-if ambuild_version.startswith('2.1'):
-    sys.stderr.write("AMBuild 2.2 or higher is required; please update\n")
-    sys.exit(1)
+builder = run.PrepareBuild(sourcePath = sys.path[0])
 
-parser = run.BuildParser(sourcePath=sys.path[0], api='2.2')
-
-parser.options.add_argument('--hl2sdk-root', type=str, dest='hl2sdk_root', default=None,
-                       help='Root search folder for HL2SDKs')
-parser.options.add_argument('--mms-path', type=str, dest='mms_path', default=None,
+builder.options.add_option('--hl2sdk-root', type=str, dest='hl2sdk_root', default=None,
+		                   help='Root search folder for HL2SDKs')
+builder.options.add_option('--mms-path', type=str, dest='mms_path', default=None,
                        help='Path to Metamod:Source')
-parser.options.add_argument('--sm-path', type=str, dest='sm_path', default=None,
+builder.options.add_option('--sm-path', type=str, dest='sm_path', default=None,
                        help='Path to SourceMod')
-parser.options.add_argument('--enable-debug', action='store_const', const='1', dest='debug',
+builder.options.add_option('--enable-debug', action='store_const', const='1', dest='debug',
                        help='Enable debugging symbols')
-parser.options.add_argument('--enable-optimize', action='store_const', const='1', dest='opt',
+builder.options.add_option('--enable-optimize', action='store_const', const='1', dest='opt',
                        help='Enable optimization')
-parser.options.add_argument('-s', '--sdks', default='all', dest='sdks',
+builder.options.add_option('-s', '--sdks', default='all', dest='sdks',
                        help='Build against specified SDKs; valid args are "all", "present", or '
-                            'comma-delimited list of engine names')
-parser.options.add_argument('--targets', type=str, dest='targets', default=None,
-                       help='Override the target architecture (use commas to separate multiple targets).')
+                            'comma-delimited list of engine names (default: %default)')
 
-parser.Configure()
+builder.Configure()
